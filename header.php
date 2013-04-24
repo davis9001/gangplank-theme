c7fa9ec14cf1364aedbe0f0036cf542d2ee61fa3
David Monaghan <github@allstruck.com>
2013-04-23 20:45:32 -0700

    Added "main_menu" theme locaton for navbar menu.

--- header.php
+++ header.php
@@ -26,25 +26,26 @@
 <body <?php body_class(); ?>>
 
 <div class="navbar navbar-inverse navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="brand" href="#">Gangplank</a>
-			<div class="nav-collapse collapse">
-				<ul class="nav">
-				<?
-						$args['container'] = '';
-						$args['container_class'] = '';
-						$args['menu_class'] = 'main-menu';
-						$args['items_wrap'] = '%3$s';
-						$args['walker'] = new menu_walker();
-						wp_nav_menu($args)
-					?>
-				  <li class="dropdown">
+  <div class="navbar-inner">
+    <div class="container">
+      <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="brand" href="#">Gangplank</a>
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+        <?
+            $args['theme_location'] = 'main_menu';
+            $args['container'] = '';
+            $args['container_class'] = '';
+            $args['menu_class'] = 'main-menu';
+            $args['items_wrap'] = '%3$s';
+            $args['walker'] = new menu_walker();
+            wp_nav_menu($args);
+          ?>
+          <li class="dropdown">
             <a id="drop1" href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">Locations <b class="caret"></b></a>
             <ul class="dropdown-menu" role="menu" aria-labelledby="drop1">
               <li role="presentation"><a role="menuitem" tabindex="-1" href="/locations/avondale">Avondale, Arizona</a></li>
@@ -54,10 +55,10 @@
               <li role="presentation"><a role="menuitem" tabindex="-1" href="/locations/tucson">Tucson, Arizona</a></li>
             </ul>
           </li>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</div>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </div>
 </div>
 <div class="container-fluid">
   <header class="header">
@@ -78,8 +79,8 @@
         <h1><?php echo get_post_meta($post->ID, 'gp_hero_title', true); ?></h1>
         <p>
           <span class="promo">
-          	<?php echo get_post_meta($post->ID, 'gp_hero_subtitle', true); ?>
-          	<?php if (get_post_meta($post->ID, 'gp_hero_button_url', true)) { ?>
+            <?php echo get_post_meta($post->ID, 'gp_hero_subtitle', true); ?>
+            <?php if (get_post_meta($post->ID, 'gp_hero_button_url', true)) { ?>
               <a class="btn btn-primary btn-large" href="<?php echo get_post_meta($post->ID, 'gp_hero_button_url', true); ?>"><?php echo get_post_meta($post->ID, 'gp_hero_button_title', true); ?></a>
             <?php } ?>
           </span>
